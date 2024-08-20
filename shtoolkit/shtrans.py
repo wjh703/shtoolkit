9c76122cf5ea94bc6c58e4b28e0737fe88313221
wjh <joycug@163.com>
2024-08-21 00:33:17 +0800

    Update shtrans and test_sle

--- shtoolkit/shtrans.py
+++ shtoolkit/shtrans.py
@@ -1,24 +1,27 @@
-import numpy as np
-from pyshtools.expand import MakeGridDH, SHExpandDH # type: ignore
+import numpy as _np
+from pyshtools.expand import (      
+        MakeGridDH as _MakeGridDH,  
+        SHExpandDH as _SHExpandDH 
+    )
 
 
 __all__ = ['cilm2grid', 'grid2cilm']
 
 
 def cilm2grid(
-        cilm: np.ndarray,
+        cilm: _np.ndarray,
         resol: int | None = None,
         lmax_calc: int | None = None,
         sampling: int = 2
-    ) -> np.ndarray:
-    grid = MakeGridDH(cilm, sampling=sampling, lmax=resol, lmax_calc=lmax_calc, extend=False)
+    ) -> _np.ndarray:
+    grid = _MakeGridDH(cilm, sampling=sampling, lmax=resol, lmax_calc=lmax_calc, extend=False)
     return grid
 
 
 def grid2cilm(
-        grd: np.ndarray,
+        grd: _np.ndarray,
         lmax_calc: int | None = None,
         sampling: int = 2
-    ) -> np.ndarray:
-    cilm = SHExpandDH(grd, sampling=sampling, lmax_calc=lmax_calc)
+    ) -> _np.ndarray:
+    cilm = _SHExpandDH(grd, sampling=sampling, lmax_calc=lmax_calc)
     return cilm
