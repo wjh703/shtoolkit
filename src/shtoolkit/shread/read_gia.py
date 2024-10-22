1b8f21612849536ea85c97fcc391db19d8112261
wjh <joycug@163.com>
2024-10-22 17:47:52 +0800

    add ICE6G-C option in read_gia_model

--- src/shtoolkit/shread/read_gia.py
+++ src/shtoolkit/shread/read_gia.py
@@ -4,23 +4,33 @@ from pathlib import Path
 import numpy as np
 
 
-def read_gia_model(filepath: str | Path, lmax: int, model: str = "P18") -> np.ndarray:
-    """read GIA model, only support ICE6G-D"""
+def read_gia_model(filepath: str | Path, lmax: int, model: str) -> np.ndarray:
     gia = np.zeros((2, lmax + 1, lmax + 1), dtype=np.float64)
 
     with open(filepath, "r", encoding="utf-8") as f:
         content = f.read()
 
-    if model == "ICE6G-D":
-        regex_deg_0_to_2 = (
-            r"GRACE Approximation for degrees 0 to 2\s+((?:\s*\d+\s+\d+\s+[+\-\d\.E]+\s+[+\-\d\.E]+\s*)+)"
-        )
-        regex_deg_greater_2 = (
-            r"GRACE Approximation/Absolute Sea-level Values for degrees > 2\s+"
-            r"((?:\s*\d+\s+\d+\s+[+\-\d\.E]+\s+[+\-\d\.E]+\s*)+)"
-        )
+    if model in ["ICE6G-D", "ICE6G-C"]:
+        if model == "ICE6G-D":
+            regex_deg_0_to_2 = (
+                r"GRACE Approximation for degrees 0 to 2\s+((?:\s*\d+\s+\d+\s+[+\-\d\.E]+\s+[+\-\d\.E]+\s*)+)"
+            )
+            regex_deg_greater_2 = (
+                r"GRACE Approximation/Absolute Sea-level Values for degrees > 2\s+"
+                r"((?:\s*\d+\s+\d+\s+[+\-\d\.E]+\s+[+\-\d\.E]+\s*)+)"
+            )
+        else :
+            regex_deg_0_to_2 = (
+                r"GRACE Coefficients for degrees 0 to 2\s+((?:\s*\d+\s+\d+\s+[+\-\d\.E]+\s+[+\-\d\.E]+\s*)+)"
+            )
+            regex_deg_greater_2 = (
+                r"GRACE/GEOID Velocity Coefficients for degrees > 2"
+                r"((?:\s*\d+\s+\d+\s+[+\-\d\.E]+\s+[+\-\d\.E]+\s*)+)"
+            )
+
         deg_0_to_2 = re.findall(regex_deg_0_to_2, content)
         deg_greater_2 = re.findall(regex_deg_greater_2, content)
+
         if deg_0_to_2 and deg_greater_2:
             deg_full = deg_0_to_2[0] + deg_greater_2[0]
         else:
@@ -37,6 +47,9 @@ def read_gia_model(filepath: str | Path, lmax: int, model: str = "P18") -> np.nd
             if l > lmax:
                 continue
             gia[:, l, m] = float(ls[2]), float(ls[3])
+
+    elif model == 'C18':
+        regex_deg_full = r""
     else:
         msg = f"Invalid value of GIA model <{model}>."
         raise ValueError(msg)
