f66e00b138ade77902d4808fa25386bde453bcae
wjh <joycug@163.com>
2024-10-28 14:28:41 +0800

    fix deg1 shape

--- src/shtoolkit/shread/read_technical_note.py
+++ src/shtoolkit/shread/read_technical_note.py
@@ -40,26 +40,24 @@ def read_technical_note_deg1(filepath: str | Path):
         content = f.read()
 
     data_regex = (
-        r"GRCOF2\s+(\d+)\s+(\d+)\s+([+\-\d\.Ee]+)\s+([+\-\d\.Ee]+)\s+([+\-\d\.Ee]+)\s+([+\-\d\.Ee]+)\s+(\d{8})\.\d{4}\s+(\d{8})\.\d{4}\n"
-        r"GRCOF2\s+(\d+)\s+(\d+)\s+([+\-\d\.Ee]+)\s+([+\-\d\.Ee]+)\s+([+\-\d\.Ee]+)\s+([+\-\d\.Ee]+)\s+\d{8}\.\d{4}\s+\d{8}\.\d{4}"
+        r"GRCOF2\s+\d+\s+\d+\s+([+\-\d\.Ee]+)\s+[+\-\d\.Ee]+\s+([+\-\d\.Ee]+)\s+[+\-\d\.Ee]+\s+(\d{8})\.\d{4}\s+(\d{8})\.\d{4}\n"
+        r"GRCOF2\s+\d+\s+\d+\s+([+\-\d\.Ee]+)\s+([+\-\d\.Ee]+)\s+([+\-\d\.Ee]+)\s+([+\-\d\.Ee]+)\s+\d{8}\.\d{4}\s+\d{8}\.\d{4}"
     )
 
     data_matches = re.findall(data_regex, content)
-    cilm = np.zeros((len(data_matches), 2, 2, 2))
-    ecilm = np.zeros((len(data_matches), 2, 2, 2))
+    deg1 = np.zeros((len(data_matches), 3))
+    deg1_std = np.zeros((len(data_matches), 3))
     epochs = np.zeros(len(data_matches))
 
     for i, match in enumerate(data_matches):
-        l1, m1, clm1, slm1, clm_std1, slm_std1, start_time, end_time = match[:8]
-        l2, m2, clm2, slm2, clm_std2, slm_std2 = match[8:]
-        cilm[i, :, int(l1), int(m1)] = float(clm1), float(slm1)
-        cilm[i, :, int(l2), int(m2)] = float(clm2), float(slm2)
-        ecilm[i, :, int(l1), int(m1)] = float(clm_std1), float(slm_std1)
-        ecilm[i, :, int(l2), int(m2)] = float(clm_std2), float(slm_std2)
+        c10, c10_std, start_time, end_time = match[:4]
+        c11, s11, c11_std, s11_std = match[4:]
+        deg1[i] = c10, c11, s11
+        deg1_std[i] = c10_std, c11_std, s11_std
 
         start = shtime.date_to_decimal_year(start_time)
         end = shtime.date_to_decimal_year(end_time)
         epoch = (start + end) / 2
         epochs[i] = epoch
 
-    return epochs, cilm, ecilm
+    return epochs, deg1, deg1_std
